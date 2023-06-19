d252c27042f7a2b7dd97c649fb59615934345b84
danardi07 <danar.adi.ft22@mail.umy.ac.id>
2023-06-19 15:58:45 +0700

    include isotream, exceptionx array

--- exeptionStandard/exeptionStandard.cpp
+++ exeptionStandard/exeptionStandard.cpp
@@ -3,7 +3,7 @@
 //untuk obyek exeption yang akan digunakan
 #include <array>
 //untuk obyek array yang akan kita gunakan
-using namespace std;
+using namespace std; 
 
 int main() {
 	cout << "Awal Program" << endl; //penanda 1:...
