4e5fb16261197b58f52495c08c4d2ddbc2527684
danardi07 <danar.adi.ft22@mail.umy.ac.id>
2023-06-19 15:48:59 +0700

    loop untuk menulis

--- StreamWriterReader_172_/StreamWriterReader_172_.cpp
+++ StreamWriterReader_172_/StreamWriterReader_172_.cpp
@@ -15,7 +15,7 @@ int main() {
 
 	//unlimited loop untuk menulis
 	while(true) {
-		cout << "- ";
+		cout << "- "; 
 		//mendapatkan setiap karakter dalam satu baris
 		getline(cin, baris);
 		//loop akan berhenti jika anda memasukkan karakter q
